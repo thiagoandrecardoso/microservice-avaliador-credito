f99e85ddbbcda7396fcffb80cadf6712aaa27993
Thiago <thiagoandrecardoso@icloud.com>
2022-07-30 13:42:44 -0300

    Adicionado endpoint para o mscards

--- src/main/java/programadorwho/msavaliadorcredito/applicatiion/AvaliadorCreditoService.java
+++ src/main/java/programadorwho/msavaliadorcredito/applicatiion/AvaliadorCreditoService.java
@@ -3,23 +3,30 @@ package programadorwho.msavaliadorcredito.applicatiion;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import programadorwho.msavaliadorcredito.domain.model.CartaoCliente;
 import programadorwho.msavaliadorcredito.domain.model.DadosCliente;
 import programadorwho.msavaliadorcredito.domain.model.SituacaoCliente;
+import programadorwho.msavaliadorcredito.infra.clients.CartoesResourceClient;
 import programadorwho.msavaliadorcredito.infra.clients.ClienteResourceClient;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AvaliadorCreditoService {
 
     private final ClienteResourceClient clienteResourceClient;
+    private final CartoesResourceClient cartoesResourceClient;
 
     public SituacaoCliente obtersituacaoCliente(String cpf) {
 
         ResponseEntity<DadosCliente> dadosClienteResponseEntity = clienteResourceClient.datasClient(cpf);
+        ResponseEntity<List<CartaoCliente>> cartoesResponseEntity = cartoesResourceClient.getCardByClient(cpf);
 
         return SituacaoCliente
                 .builder()
                 .cliente(dadosClienteResponseEntity.getBody())
+                .cartoes(cartoesResponseEntity.getBody())
                 .build();
     }
 }
---
+++ src/main/java/programadorwho/msavaliadorcredito/infra/clients/CartoesResourceClient.java
@@ -0,0 +1,16 @@
+package programadorwho.msavaliadorcredito.infra.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import programadorwho.msavaliadorcredito.domain.model.CartaoCliente;
+
+import java.util.List;
+
+@FeignClient(value = "mscards", path="/cartoes")
+public interface CartoesResourceClient {
+
+    @GetMapping(params = "cpf")
+    ResponseEntity<List<CartaoCliente>> getCardByClient(@RequestParam("cpf") String cpf);
+}
